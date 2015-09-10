bae24510b92eed9632c421881c2b7cb6c4cb40a5
etisab <ariasabeti@gmail.com>
2015-09-10 12:55:45 -0700

    Prolog day 1

---
+++ Prolog/day1/authors.pl
@@ -0,0 +1,6 @@
+author('bruce tate', '7 languages in 7 weeks').
+author('aviva chomsky', undocumented).
+author('imaginary person', undocumented).
+
+%author('bruce tate', B).
+%author(A, undocumented).
---
+++ Prolog/day1/musicians.pl
@@ -0,0 +1,10 @@
+instrument('Colin Meloy', guitar).
+instrument('Colin Meloy', vocals).
+instrument('Robert Del Naja', vocals).
+instrument('Robert Del Naja', synth).
+
+genre('Colin Meloy', folk).
+genre('Colin Meloy', rock).
+genre('Robert Del Naja', industrial).
+genre('Robert Del Naja', rock).
+genre('Robert Del Naja', underground).
