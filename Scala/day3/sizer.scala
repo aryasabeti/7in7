da7431301fc40df390d861fddb2b1457df9d8c5f
etisab <ariasabeti@gmail.com>
2015-09-16 17:06:04 -0700

    Scala day 3 naively count URLs in page

--- Scala/day3/sizer.scala
+++ Scala/day3/sizer.scala
@@ -1,14 +1,21 @@
+// Take the sizer application and add a message to count the number of links on the page
+
 import scala.io._
 import scala.actors._
 import Actor._
 
 object PageLoader {
 	def getPageSize(url : String) = Source.fromURL(url).mkString.length
+
+	def getPageURLCount(url : String) = {
+		val urlPattern = "http://[\\w\\d-./]*".r
+		(urlPattern findAllIn Source.fromURL(url).mkString).size
+	}
 }
 
 val urls = List(
 	"http://www.amazon.com/",
-	"http://www.twitter.com/",
+	"https://www.twitter.com/",
 	"http://www.google.com/",
 	"http://www.cnn.com/")
 
@@ -21,12 +28,13 @@ def timeMethod(method: () => Unit) = {
 
 def getPageSizeSequentially() = {
 	for(url <- urls) {
-	println("Size for " + url + ": " + PageLoader.getPageSize(url))
+		println("Size for " + url + ": " + PageLoader.getPageSize(url))
+	}
 }
 
-}
 def getPageSizeConcurrently() = {
 	val caller = self
+
 	for(url <- urls) {
 		actor { caller ! (url, PageLoader.getPageSize(url)) }
 	}
@@ -39,7 +47,24 @@ def getPageSizeConcurrently() = {
 	}
 }
 
+def getPageURLCountConcurrently() = {
+	val caller = self
+
+	for(url <- urls) {
+		actor { caller ! (url, PageLoader.getPageURLCount(url)) }
+	}
+
+	for(i <- 1 to urls.size) {
+		receive {
+			case (url, count) =>
+			println("URLs in " + url + ": " + count)
+		}
+	}
+}
+
 println("Sequential run:")
 timeMethod { getPageSizeSequentially }
 println("Concurrent run")
 timeMethod { getPageSizeConcurrently }
+println("Concurrent URL count: ")
+timeMethod { getPageURLCountConcurrently }
