89b2ddbaff54abe8b15ffa79eba276bc5362ada3
etisab <ariasabeti@gmail.com>
2015-09-17 18:37:17 -0700

    Erlang day 1 counting recursively

--- Erlang/day1/count.erl
+++ Erlang/day1/count.erl
@@ -7,6 +7,7 @@
 
 -module(count).
 -export([words/1]).
+-export([to/1]).
 
 words(Phrase) -> 1 + words_recurs(string:strip(Phrase, both)).
 
@@ -20,3 +21,11 @@ words_recurs(Stripped) ->
 
 is_space(32) -> 1;
 is_space(_) -> 0.
+
+% Write a function that uses recursion to count to 10.
+
+to(N)
+	when (N == 1) -> io:format("~p\n", [N]);
+to(N) ->
+	to(N-1),
+	io:format("~p\n", [N]).
